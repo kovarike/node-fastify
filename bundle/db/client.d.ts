8fcd29b59c91669fa57afbd58627a46ec8c0fcff
kovarike <danilokovarike@gmail.com>
2025-09-21 23:34:20 -0300

    remove bundle

--- bundle/db/client.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const db: import("drizzle-orm/node-postgres").NodePgDatabase<Record<string, never>> & {
-    $client: import("drizzle-orm/node-postgres").NodePgClient extends TClient ? Pool : TClient;
-};
-//# sourceMappingURL=client.d.ts.map
